7b4363b6a9de0816da60cd707aef190536c326c9
VerstakGit <43673819+VerstakGit@users.noreply.github.com>
2018-12-04 14:41:03 +0300

    added dataGridView

--- git22/Form1.cs
+++ git22/Form1.cs
@@ -15,6 +15,14 @@ namespace git22
         public Form1()
         {
             InitializeComponent();
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.Rows.Add(1, "Преступление и наказание", "10", "1000");
+            dataGridView1.Rows.Add(2, "Война и мир", "20", "900");
+            dataGridView1.Rows.Add(3, "Отцы и дети", "15", "500");
+            dataGridView1.Rows.Add(4, "Мастер и Маргарита", "12", "600");
+            dataGridView1.Rows.Add(5, "Горе от ума", "18", "1100");
+            dataGridView1.Rows.Add(6, "Мертвые души", "14", "250");
+            dataGridView1.Rows.Add(7, "Обломов", "12", "720");
         }
     }
 }
